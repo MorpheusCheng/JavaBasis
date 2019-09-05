f95f01e54a276de66ea6befadb8d8316b19c80ec
cyz <cookiecheng@live.com>
2019-09-05 16:36:39 +0800

    完成hashmap部分

--- src/session5/BananaHashMap.java
+++ src/session5/BananaHashMap.java
@@ -9,18 +9,40 @@ public class BananaHashMap<K,V> extends BananaMap<K,V> {
     static final int DEFAULT_INITIAL_CAPACITY =  1 << 4;
     static final int MAXIMUM_CAPACITY = 1 << 30 ;
     static final float DEFAULT_LOAD_FACTOR = 0.75f;
-    private float loadFactor;
     private int threshold;
-    private int capacity;
     private int size;
+    private float loadFactor;
     static class Entry<K,V> {
-        final K key;
-        V value;
-        private Entry next;
-        private Entry prev;
+        private K key;
+        private V value;
+        private Entry<K,V> next;
         public Entry(K key,V value){
             this.key = key;
             this.value = value;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public void setNext(Entry next) {
+            this.next = next;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        public Entry getNext() {
+            return next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
         }
     }
     private Entry<K,V>[] table;
@@ -31,11 +53,9 @@ public class BananaHashMap<K,V> extends BananaMap<K,V> {
         this(capacity,DEFAULT_LOAD_FACTOR);
     }
     public BananaHashMap(int capacity,float loadFactor){
-        table = (Entry<K,V>[]) new Entry[capacity];
-        loadFactor = loadFactor;
+        this.table = new Entry[capacity];
         threshold = (int) (capacity * loadFactor);
-        capacity = capacity;
-        size = 0;
+        loadFactor = loadFactor;
     }
     @Override
     int size() {
@@ -49,31 +69,96 @@ public class BananaHashMap<K,V> extends BananaMap<K,V> {
 
     @Override
     boolean containsKey(K key) {
-        return false;
+        if (get(key) == null){
+            return false;
+        }
+        return true;
     }
 
     private static final int hash(Object key){
         int h;
-        if (key == null){
-            return 0;
-        }else {
-            h = key.hashCode();
-            return 1;
-        }
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
     }
 
     @Override
     V get(K key) {
+        int hash = hash(key);
+        int index = hash % table.length;
+        Entry<K,V> cur = table[index];
+        if (cur == null){
+            return null;
+        }
+        while (cur != null){
+            if (cur.getKey() == key || key.equals(cur.getKey())){
+                return cur.getValue();
+            }
+            cur = cur.getNext();
+        }
         return null;
     }
 
     @Override
-    V put(K key, V value) {
-        return null;
+    boolean put(K key, V value) {
+        int index = hash(key) % table.length;
+        for (Entry<K,V> e = table[index]; e != null ; e = e.getNext()){
+            if (e.getKey() == key || key.equals(e.getKey())){
+                e.setValue(value);
+                return true;
+            }
+        }
+        size++;
+        resize();
+        int newIndex = hash(key) % table.length;
+        Entry<K,V> entry = new Entry<>(key,value);
+        entry.setNext(table[newIndex]);
+        table[newIndex] = entry;
+        return false;
+    }
+    public void resize(){
+        if (size > threshold){
+            Entry[] oldtable = table;
+            if (oldtable.length == MAXIMUM_CAPACITY){
+                threshold = Integer.MAX_VALUE;
+                return;
+            }
+            Entry[] newtable = new Entry[2 * oldtable.length];
+            for (Entry<K,V> e : table){
+                while (null != e){
+                    Entry<K,V> next = e.getNext();
+                    int index = hash(e.getKey()) % newtable.length;
+                    //采用头插法插入新的表
+                    e.setNext(newtable[index]);
+                    newtable[index] = e;
+                    e = next;
+                }
+            }
+            table = newtable;
+            threshold = (int) Math.min(newtable.length * loadFactor,MAXIMUM_CAPACITY);
+        }
     }
 
     @Override
     V remove(K key) {
-        return null;
+        int index = hash(key) % table.length;
+        Entry<K, V> pre = table[index];
+        if (pre == null){
+            throw new RuntimeException("key is not exist!");
+        }
+        if (key.equals(pre.getKey()) || key == pre.getKey()) {
+            V value = pre.getValue();
+            table[index] = pre.getNext();
+            size--;
+            return value;
+        }
+        while (pre.getNext() != null) {
+            if (pre.getNext().getKey() == key || key.equals(pre.getNext().getKey())) {
+                V value = (V) pre.getNext().getValue();
+                pre = pre.getNext().getNext();
+                size--;
+                return value;
+            }
+            pre = pre.getNext();
+        }
+        throw new RuntimeException("key is not exist!");
     }
 }
