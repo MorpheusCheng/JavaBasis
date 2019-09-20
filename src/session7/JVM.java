b79487f780e56b44ad244295fc814e6b8d224352
cyz <cookiecheng@live.com>
2019-09-20 17:54:23 +0800

    初步完成session7

--- src/session7/JVM.java
+++ src/session7/JVM.java
@@ -43,6 +43,16 @@ public class JVM {
         this.survivor_size = Xmn - eden_size;
         this.from = eden_size;
         this.to = from + survivor_size / 2;
+
+        for (int i = 0; i < Xmn; i++) {
+            YOUNG_GEN_SPACE.add(i,null);
+        }
+        for (int i = 0; i < (Xmx - Xmn); i++) {
+            OLD_GEN_SPACE.add(i,null);
+        }
+        for (int i = 0; i < XXPermSize; i++) {
+            PERM_GEN_SPACE.add(i,null);
+        }
     }
 
 
@@ -81,10 +91,11 @@ public class JVM {
         }else {
             GC_ROOT.put(name,1);
             JMemory newJMemory = new JMemory();
-            newJMemory.setObject(value);
+            //这个地方有点问题，如何实现GC_ROOT中的对象与内存区对象之间的对应？ 目前暂定用name来解决
+            newJMemory.setObject(name);
             newJMemory.setGcCount(0);
             newJMemory.setMark(false);
-            YOUNG_GEN_SPACE.set(getEmptySpace(YOUNG_GEN_SPACE,eden_size),newJMemory);
+            YOUNG_GEN_SPACE.set(getEmptySpace(YOUNG_GEN_SPACE,0,eden_size),newJMemory);
         }
 
     }
@@ -92,21 +103,45 @@ public class JVM {
     public void removeObject(String name){
         gc();
         if (!GC_ROOT.containsKey(name)){
-            throw new RuntimeException("没有此对象: "+name);
+            System.out.println("没有此对象：" + name);
         }else {
-            GC_ROOT.put(name,0);
+            //对于remove操作暂时认定为减少对该对象的引用一次
+            GC_ROOT.put(name,GC_ROOT.get(name) - 1);
+            if (GC_ROOT.get(name) <= 0){
+                for (int i = 0; i < YOUNG_GEN_SPACE.size(); i++) {
+                    if (YOUNG_GEN_SPACE.get(i) == null){
+                        continue;
+                    }
+                    if (YOUNG_GEN_SPACE.get(i).getObject().equals(name)){
+                        YOUNG_GEN_SPACE.get(i).setMark(true);
+                        GC_ROOT.remove(name);
+                        return;
+                    }
+                }
+                for (int i = 0; i < OLD_GEN_SPACE.size(); i++) {
+                    if (OLD_GEN_SPACE.get(i) == null){
+                        continue;
+                    }
+                    if (OLD_GEN_SPACE.get(i).getObject().equals(name)){
+                        OLD_GEN_SPACE.get(i).setMark(true);
+                        GC_ROOT.remove(name);
+                        return;
+                    }
+                }
+            }
         }
     }
 
 
     public  void addPermObject(String name , Object value){
-        int index = getEmptySpace(PERM_GEN_SPACE,PERM_GEN_SPACE.size());
+        int index = getEmptySpace(PERM_GEN_SPACE,0,PERM_GEN_SPACE.size());
         if(index == -1){
             gc();
         }
         GC_ROOT.put(name,1);
         JMemory newJMermory = new JMemory();
-        newJMermory.setObject(value);
+        //另一个问题永久区的对象和新生代的对象如何在GC_ROOT中区分开来 这里暂时使用name的开头进行区分
+        newJMermory.setObject(name);
         newJMermory.setGcCount(0);
         newJMermory.setMark(false);
         PERM_GEN_SPACE.set(index,newJMermory);
@@ -115,13 +150,22 @@ public class JVM {
     public  void removePermObject(String name){
         gc();
         if (!GC_ROOT.containsKey(name)){
-            throw new RuntimeException("没有此对象：" + name);
+            System.out.println("没有此对象：" + name);
         }else {
-            GC_ROOT.put(name,0);
+            GC_ROOT.put(name,GC_ROOT.get(name) - 1);
+
+            if (GC_ROOT.get(name) <= 0){
+                for (JMemory element : PERM_GEN_SPACE){
+                    if (element.getObject().equals(name)){
+                        element.setMark(true);
+                        GC_ROOT.remove(name);
+                        return;
+                    }
+                }
+            }
         }
 
     }
-
 
     /**
      * 这里自己实现 gc 算法，比如什么时候应该进行代的晋升，什么时候进行young gc，什么时候进行full gc，什么时候应该crash。
@@ -136,14 +180,111 @@ public class JVM {
     private  void gc(){
         if (isEdenFull()){
             //执行minor gc
-            for (int i = 0; i < eden_size ; i++) {
+            /*
+            当Eden区满的时候执行Minor gc
+            Minor gc期间 将Mark为false的对象清理掉 并将没有清理掉的对象地gcCounts自增
+            将老对象移到survivor区，如果from满了，直接移入到老年代，将to与from互换
+            survivor区的老对象要移到老年代
+             */
+            System.out.println("执行Minor gc");
+            //清理被标记的垃圾 所有的新生代的对象
+            for (int i = 0; i < YOUNG_GEN_SPACE.size() ; i++) {
+                if (YOUNG_GEN_SPACE.get(i) == null){
+                    continue;
+                }
+                if (YOUNG_GEN_SPACE.get(i).isMark()){
+                    YOUNG_GEN_SPACE.set(i,null);
+                }else {
+                    //存活对象年龄加一
+                    YOUNG_GEN_SPACE.get(i).setGcCount(YOUNG_GEN_SPACE.get(i).getGcCount() + 1);
+                }
+            }
+            //将老对象移入到survivor区
+            int sur = 0;
+            for (int i = 0; i < eden_size; i++) {
+                if (YOUNG_GEN_SPACE.get(i) != null){
+                    sur++;
+                }
+            }
+            //from区的剩余大小 设计两个survivor区的目的是避免内存的碎片化
+            int fromSur = 0;
+            for (int i = from; i < (survivor_size / 2); i++) {
+                //survivor区中年龄高于3的直接进入old区
+                if (YOUNG_GEN_SPACE.get(i) == null){
+                    continue;
+                }
+                if (YOUNG_GEN_SPACE.get(i).getGcCount() > 3){
+                    int j = getEmptySpace(OLD_GEN_SPACE,0,OLD_GEN_SPACE.size());
+                    OLD_GEN_SPACE.set(j , YOUNG_GEN_SPACE.get(i));
+                    YOUNG_GEN_SPACE.set(i,null);
+                }
+                else if (YOUNG_GEN_SPACE.get(i) != null){
+                    fromSur++;
+                }
+            }
+            if (sur > fromSur){
+                //survivor区无法放入，直接放入老年代
+                for (int i = 0; i < eden_size; i++) {
+                    if (YOUNG_GEN_SPACE.get(i) != null){
+                        int j = getEmptySpace(OLD_GEN_SPACE,0,OLD_GEN_SPACE.size());
+                        if (j != -1){
+                            OLD_GEN_SPACE.set(j,YOUNG_GEN_SPACE.get(i));
+                            YOUNG_GEN_SPACE.set(i,null);
+                        }else {
+                            break;
+                        }
+                    }
+                }
+            }else {
+                //survivor可以放入
+                for (int i = 0; i < eden_size; i++) {
+                    if (YOUNG_GEN_SPACE.get(i) != null){
+                        int j = getEmptySpace(YOUNG_GEN_SPACE,to,survivor_size / 2);
+                        YOUNG_GEN_SPACE.set(j , YOUNG_GEN_SPACE.get(i));
+                        YOUNG_GEN_SPACE.set(i,null);
+                    }
+                }
+                for (int i = from; i < (survivor_size / 2); i++) {
+                    if (YOUNG_GEN_SPACE.get(i) != null){
+                        int j = getEmptySpace(YOUNG_GEN_SPACE,to,survivor_size / 2);
+                        YOUNG_GEN_SPACE.set(j , YOUNG_GEN_SPACE.get(i));
+                        YOUNG_GEN_SPACE.set(i,null);
+                    }
+                }
+                //交换from 和 to 指针
+                int temp = from;
+                from = to;
+                to = temp;
+            }
+
+        }
+        /*
+        执行full gc
+
+         */
+        if (isOldGenFull() || isPermFull()){
+            System.out.println("执行full gc");
+            for (int i = 0; i < YOUNG_GEN_SPACE.size(); i++) {
+                if (YOUNG_GEN_SPACE.get(i) == null){
+                    continue;
+                }
                 if (YOUNG_GEN_SPACE.get(i).isMark()){
                     YOUNG_GEN_SPACE.set(i,null);
                 }else {
                     YOUNG_GEN_SPACE.get(i).setGcCount(YOUNG_GEN_SPACE.get(i).getGcCount() + 1);
                 }
             }
-            return;
+            for (int i = 0; i < OLD_GEN_SPACE.size(); i++) {
+                if (OLD_GEN_SPACE.get(i) == null){
+                    continue;
+                }
+                if (OLD_GEN_SPACE.get(i).isMark()){
+                    OLD_GEN_SPACE.set(i,null);
+                }else {
+                    OLD_GEN_SPACE.get(i).setGcCount(OLD_GEN_SPACE.get(i).getGcCount() + 1);
+                }
+            }
+
         }
 
 
@@ -166,7 +307,7 @@ public class JVM {
     }
 
     private boolean isEdenFull(){
-        for (int i = 0 ; i < eden_size ; i++){
+        for (int i = 0; i < eden_size; i++) {
             if (YOUNG_GEN_SPACE.get(i) == null){
                 return false;
             }
@@ -181,9 +322,17 @@ public class JVM {
         }
         return true;
     }
+    private boolean isPermFull(){
+        for (JMemory element : PERM_GEN_SPACE){
+            if (element == null){
+                return false;
+            }
+        }
+        return true;
+    }
 
-    private int getEmptySpace(List<JMemory> space,int size){
-        for (int i = 0; i < size; i++) {
+    private int getEmptySpace(List<JMemory> space,int start,int size){
+        for (int i = start; i < size; i++) {
             if (space.get(i) == null){
                 return i;
             }
