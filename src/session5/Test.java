214d3684d59338f58dcbfac2b88218f6be9de2ba
cheng <1203969904@qq.com>
2019-10-04 15:12:36 +0800

    test

--- src/session5/Test.java
+++ src/session5/Test.java
@@ -1,5 +1,7 @@
 package session5;
 
+import java.util.HashMap;
+
 /**
  * Author: cyz
  * Date: 2019/9/3
@@ -7,6 +9,17 @@ package session5;
  */
 public class Test {
     public static void main(String[] args) {
+        String name = "123";
+        HashMap hashMap = new HashMap();
+        System.out.println(gethash(name));
+        System.out.println(name.hashCode());
+        System.out.println(name.hashCode() >>> 16);
+    }
+    public static int gethash(String name){
+        int h;
+        return (name == null) ? 0 : (h = name.hashCode()) ^ (h >>> 16);
+    }
+    public void test(){
         BananaHashMap<String,String> map = new BananaHashMap<>(2,1);
         System.out.println(map.size());
         map.put("A","A");
